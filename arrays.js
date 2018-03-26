d75b073ed4e0ab2e7f26d5a721cdadd0191276f3
grant diamond <grant.a.diamond@gmail.com>
2018-03-26 17:19:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   array.slice(1)
 }
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+}
