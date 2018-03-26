472f6cf2c1f698fa2b97c9ad20991b466e0af491
grant diamond <grant.a.diamond@gmail.com>
2018-03-26 17:08:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,17 +2,17 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 
 function addElementToBeginningOfArray(array, element){
-  [element, ...array]
+  return [element, ...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element){
-  [...array, element]
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element){
-  array.push(element)
+  return array.push(element);
 }
