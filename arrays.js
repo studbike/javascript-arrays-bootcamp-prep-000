9f7eec0537482476cc20228718bca11fe7677f29
grant diamond <grant.a.diamond@gmail.com>
2018-03-26 17:13:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,7 @@ function destructivelyAddElementToEndOfArray(array, element){
 function accessElementInArray(array, index){
   return array[index]
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  shift.array()
+  return array
+}
